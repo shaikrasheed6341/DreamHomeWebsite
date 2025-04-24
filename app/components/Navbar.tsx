c643cf2a164967d9cd35706b7deb8515a21b08a7
shaikrasheed <shaikrasheed6341@gmail.com>
2025-04-24 11:05:59 +0530

    added a Navbar

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,23 @@
+"use client"
+import { FaSearch } from "react-icons/fa";
+import Link from "next/link";
+const Navbar= ()=>{
+    const lists =[{id:1,value:"Home" },{id:2,value:"Service"},{id:3,value:"Agent"},{id:4,value:"Contact"}];
+    return(
+        <nav className="flex  justify-between bg-[#FEF7F2] mx-7   sticky p-3">
+            <div >logo</div>,
+            <ul className="space-x-8  text-[#2B1B12] flex    ">
+               {lists.map(list =>  <li  className="font-semibold" key={list.id}>{  list.value}</li>)}
+               
+            </ul>
+            <div className="flex space-x-10">
+                <div className="text-[#2B1B12] mt-2 "><FaSearch className="text-xl" /></div>
+                <div><button className="text-white px-5 py-2 rounded-xl text-sm bg-[#2B1B12]  " > <Link href={"/signup"}>Signup</Link> </button></div>
+
+            </div>
+
+        </nav>
+    )
+} 
+
+export default Navbar;
---
+++ app/signup/page.tsx
@@ -0,0 +1,7 @@
+const signup = ()=>{
+
+    return(
+        <div>heelo signup</div>
+    )
+}
+export default signup
