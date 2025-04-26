0de1b503d89e1b2a95daa3ec66ae00d8df0edb1e
shaikrasheed <shaikrasheed6341@gmail.com>
2025-04-26 09:15:08 +0530

    done with help line componnet

--- app/Supportcards/page.tsx
+++ app/Supportcards/page.tsx
@@ -1,11 +1,21 @@
 import { Supportcard } from "../components/Supportcard/Supportcard";
-const Supportcards =()=>{
-    return(
-        <div className=" grid sm:grid-cols-1 md:grid-cols-3 p-5 lg:grid-cols-4 ">
-            <div className="m-2"><Supportcard tittle={"hello"} icons={"asdfa"} descripation={"asdfasdf"} /></div>
-            <div className="m-2"><Supportcard tittle={"hello"} icons={"asdfa"} descripation={"asdfasdf"} /></div>
-            <div className="m-2"><Supportcard tittle={"hello"} icons={"asdfa"} descripation={"asdfasdf"} /></div>
-            <div className="m-2"><Supportcard tittle={"hello"} icons={"asdfa"} descripation={"asdfasdf"} /></div>
+import { IoLocationSharp } from "react-icons/io5";
+import { MdPersonSearch } from "react-icons/md";
+import { BiSolidNotepad } from "react-icons/bi";
+import { FaHandshakeSimple } from "react-icons/fa6";
+
+
+
+
+const Supportcards = () => {
+    return (
+        <div className=" grid sm:grid-cols-1 md:grid-cols-3 p-5 lg:grid-cols-4 bg-[#FBF5F1] ">
+            <div className="m-2"><Supportcard tittle={"Expert Guidance"} icons={<IoLocationSharp className="" />
+            } descripation={"Benefit from our team's seasoned expertise for a smooth buying experience"} /></div>
+            <div className="m-2"><Supportcard tittle={"Personalized Service"} icons={<MdPersonSearch />
+} descripation={"Our services adapt to your unique needs, making your journey stress-free"} /></div>
+            <div className="m-2"><Supportcard tittle={"Transparent Process"} icons={<BiSolidNotepad />} descripation={"Stay informed with our clear and honest approach to buying your home"} /></div>
+            <div className="m-2"><Supportcard tittle={"Exceptional Support"} icons={<FaHandshakeSimple />} descripation={"Providing peace of mind with our responsive and attentive customer service"} /></div>
 
         </div>
     )
--- app/components/Supportcard/Supportcard.tsx
+++ app/components/Supportcard/Supportcard.tsx
@@ -1,10 +1,18 @@
+import React from "react"
 
-export  const Supportcard = ({icons,tittle,descripation})=>{
+export interface info{
+    icons :React.ReactNode;
+    tittle:string;
+    descripation:string;
+}
+export  const Supportcard:React.FC<info> = ({icons,tittle,descripation})=>{
     return(
-        <div className="bg-[#DDC7BB]">
-            <div>{icons}</div>
-            <div><h1 className="text-2xl text-[#2B1B12]">{tittle}</h1></div>
-            <div><p className="text-2xl text-[#2B1B12]">{descripation}</p></div>
+        <div >
+        <div className="bg-[#DDC7BB] p-4 rounded-xl">
+            <div className="bg-gray-100 w-max rounded-sm text-[#2B1B12] my-2  text-5xl ">{icons}</div>
+            <div><h1 className="text-2xl my-3 font-bold text-[#2B1B12]">{tittle}</h1></div>
+            <div><p className="text-md text-wrap text-[#2B1B12]">{descripation}</p></div>
+        </div>
         </div>
     )
 }
